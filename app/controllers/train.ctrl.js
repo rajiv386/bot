3608dfc0a6c3afdb0c33336492e006c4a2285e73
KUMAR <rk580v@att.com>
2017-01-27 08:23:42 -0500

    first pull

--- app/controllers/train.ctrl.js
+++ app/controllers/train.ctrl.js
@@ -11,6 +11,8 @@ angular.module('yapp').controller('TrainCtrl', TrainCtrl);
 function TrainCtrl($scope, $state) {
 
 	$scope.$state = $state;
+	$scope.cnt = 1;
+	
 	/*    $scope.list_of_files = ['File1', 'File2', 'File3', 'File4'];
 	 */
 	$scope.addnewresp = function() {
@@ -18,6 +20,10 @@ function TrainCtrl($scope, $state) {
 		var parent = document.getElementById("AddResponseForm");
 		var sib = document.getElementById("addresp");
 		var child = document.createElement("input");
+		
+		child.setAttribute("ng-model", "inputresponse"+$scope.cnt);
+		$scope.cnt += 1;
+		
 		var _br1=document.createElement("br");
 		var _br2=document.createElement("br");
 		parent.appendChild(child);
@@ -32,5 +38,23 @@ function TrainCtrl($scope, $state) {
 	$scope.showAddResponseForm = function() {
 		document.getElementById("AddResponseForm").style.visibility = "visible";
 	}
+	
+	$scope.sendInput = function() {
 
+		console.log($scope.inputtext);
+	}
+
+	
+	$scope.sendNewResponses = function() {
+
+		console.log("Counter = ",$scope.cnt);
+		for(var i=0; i<$scope.cnt;i++){
+			var a="inputresponse"+i
+			
+			console.log($scope[a],a);
+		}
+		
+	}
+
+	
 }
