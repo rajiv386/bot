d14b728079d7377be1b400a671890fbf3eeb46c7
shinigangrade <shinigangrade@gmail.com>
2017-02-01 08:09:56 +0530

    chatbox service added

--- app/controllers/train.ctrl.js
+++ app/controllers/train.ctrl.js
@@ -12,29 +12,37 @@ function TrainCtrl($scope, $state) {
 
 	$scope.$state = $state;
 	$scope.cnt = 1;
+	$scope.modal={}
+  $scope.choices = [{id: 'choice1'}];
 
-	/*    $scope.list_of_files = ['File1', 'File2', 'File3', 'File4'];
-	 */
-	$scope.addnewresp = function() {
-		console.log("aaaaa");
-		var parent = document.getElementById("AddResponseForm");
-		var sib = document.getElementById("addresp");
-		var child = document.createElement("input");
+  $scope.addNewChoice = function() {
+    var newItemNo = $scope.choices.length+1;
+    $scope.choices.push({'id':'choice'+newItemNo});
+  };
+	// $scope.addnewresp = function() {
+	// 	var parent = document.getElementById("AddResponseForm");
+	// 	var sib = document.getElementById("addresp");
+	// 	var child = document.createElement("input");
+    // $scope.modal['input'+$scope.cnt] =''
+	// 	child.setAttribute("ng-model",'input'+$scope.cnt);
+	// 	$scope.cnt += 1;
+    //
+	// 	var _br1=document.createElement("br");
+	// 	var _br2=document.createElement("br");
+	// 	parent.appendChild(child);
+	// 	parent.insertBefore(child, sib);
+	// 	parent.appendChild(_br1);
+	// 	parent.insertBefore(_br1, sib);
+	// 	parent.appendChild(_br2);
+	// 	parent.insertBefore(_br2, sib)
+    //
+    //
+	// }
+  $scope.removeChoice = function() {
+    var lastItem = $scope.choices.length-1;
+    $scope.choices.splice(lastItem);
+  };
 
-		child.setAttribute("ng-model", "inputresponse"+$scope.cnt);
-		$scope.cnt += 1;
-
-		var _br1=document.createElement("br");
-		var _br2=document.createElement("br");
-		parent.appendChild(child);
-		parent.insertBefore(child, sib);
-		parent.appendChild(_br1);
-		parent.insertBefore(_br1, sib);
-		parent.appendChild(_br2);
-		parent.insertBefore(_br2, sib)
-
-
-	}
 	$scope.showAddResponseForm = function() {
 		document.getElementById("AddResponseForm").style.visibility = "visible";
 	}
@@ -47,12 +55,13 @@ function TrainCtrl($scope, $state) {
 
 	$scope.sendNewResponses = function() {
 
-		console.log("Counter = ",$scope.cnt);
-		for(var i=0; i<$scope.cnt;i++){
-			var a="inputresponse"+i
-
-			console.log($scope[a],a);
-		}
+		console.log($scope.choices);
+		// for(var i=0; i<$scope.cnt;i++){
+		//   if(i==0)
+		// 	var a="inputresponse"+i
+        //
+		// 	console.log($scope[a],a);
+		// }
 
 	}
 
